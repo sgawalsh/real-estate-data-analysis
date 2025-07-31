b507e94d3c6e6359e15483e858807f0c93422dca
Stephen Walsh <sgawalsh@gmail.com>
2025-07-31 12:32:06 -0400

    Add readme and update main

--- main.py
+++ main.py
@@ -4,20 +4,17 @@ import pandas as pd
 def generateClusters(data: pd.DataFrame, info: pd.DataFrame):
     clustering.pcaCompare(data, info, kRange=range(1, 15, 3), stop = 0, step=15)
 
-def dbScan(data: pd.DataFrame, mapData: pd.DataFrame):
-    clustering.dbScan(data, mapData)
-
 def anomalyDetect(data: pd.DataFrame, info: pd.DataFrame, rawTargets: pd.Series):
     # anomalyDetection.mapAnomalies(anomalyDetection.isolationForest(data, info))
     preds, y, bestColumn = anomalyDetection.compareModelPreds(data)
-    preds, y = processing.descalePreds(preds, y, rawTargets.mean(), rawTargets.std())
+    preds, y = processing.descalePreds(preds, y, rawTargets)
     anomalyDetection.mapUnderpriced(preds[bestColumn], y, info)
 
 if __name__ == "__main__":
     data = pd.read_csv("data/kaggle_london_house_price_data.csv")
     rawTargets = data['saleEstimate_currentPrice'].copy()
     processed, info = processing.processData(data)
-    generateClusters(processed, info)
-    # anomalyDetect(processed, info, rawTargets)
-    # dbScan(processed, info)
-    # clustering.compareDbScanKmeansLabels(processed)
+    # generateClusters(processed, info)
+    anomalyDetect(processed, info, rawTargets)
+    # clustering.dbScan(processed, info)
+    # clustering.compareDbScanKmeansLabels(processed, showHeatmaps=True)
